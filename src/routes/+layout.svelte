d7b4459c1cf36242212e63bdd6350a28bc69e010
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2024-01-05 20:40:24 +0300

    llllllll

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,27 +2,22 @@
   import "../app.css";
   import "$lib/preline.js";
   import { afterNavigate } from "$app/navigation";
-  import { navigating } from '$app/stores'
+  import { navigating } from "$app/stores";
 
-import { loading } from '$lib/loading'
-import Loading from '$lib/Loading.svelte'
+  import { loading } from "$lib/loading";
+  import Loading from "$lib/Loading.svelte";
 
-$: $loading = !!$navigating
+  $: $loading = !!$navigating;
 
- 
-// const { afterNavigate } = await import("$app/navigation");
+  // const { afterNavigate } = await import("$app/navigation");
 
-   
-
-afterNavigate(() => {
-    window.HSStaticMethods.autoInit(); 
-  }); 
-
-
+  afterNavigate(() => {
+    window.HSStaticMethods.autoInit();
+  });
 </script>
 
 <slot />
- <button on:click={() => $loading = true} >Set Loading</button>
-<button on:click={() => $loading = false} >Stop Loading</button> 
+<button on:click={() => ($loading = true)}>Set Loading</button>
+<button on:click={() => ($loading = false)}>Stop Loading</button>
 
- <Loading />
+<Loading />
