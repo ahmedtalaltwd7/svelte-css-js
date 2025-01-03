106b90389f99e8427195b996b23bfd4eba03cb95
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2025-01-04 00:54:26 +0300

    addd

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import adapter from '@sveltejs/adapter-auto';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
+};
 
+export default config;
 
 
