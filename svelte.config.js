9d1746048ebca0510b224181f615d97052596b2b
Ahmed Talal <76573910+ahmedtalaltwd7@users.noreply.github.com>
2025-01-04 00:38:32 +0300

    adbter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-  kit: {
-    adapter: adapter()
-  },
-  preprocess: vitePreprocess()
-};
-export default config;
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [sveltekit()]
+});
+
 
 
